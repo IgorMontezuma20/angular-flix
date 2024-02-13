d64474046c8dd76d1a6a37f5f1a0d3eaf9a1bb81
IgorMontezuma20 <igorminranda17@gmail.com>
2024-02-12 23:17:35 -0300

    refactor: Change the data language to portuguese

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -8,18 +8,19 @@ import { MoviesDTO } from '../types/movie'
 export class MoviesService {
   private api_url = 'https://api.themoviedb.org/3'
   private api_key = '60dc3814ec88818f2a6b586ca2c952f5'
+  private movie_language = 'pt-BR'
 
   constructor(private http: HttpClient) {}
 
   getPopularMovies() {
     return this.http.get<MoviesDTO>(
-      `${this.api_url}/movie/popular?api_key=${this.api_key}`
+      `${this.api_url}/movie/popular?api_key=${this.api_key}&language=${this.movie_language}`
     )
   }
 
   getUpcomingMovies() {
     return this.http.get<MoviesDTO>(
-      `${this.api_url}/movie/upcoming?api_key=${this.api_key}`
+      `${this.api_url}/movie/upcoming?api_key=${this.api_key}&language=${this.movie_language}`
     )
   }
 }
