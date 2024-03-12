a3d2b55feccc85571bba9da931ee662aa83e2dde
IgorMontezuma20 <igorminranda17@gmail.com>
2024-03-12 16:41:56 -0300

    feat: Display the genres from the api

--- src/app/pages/genres/genres.component.ts
+++ src/app/pages/genres/genres.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { MoviesService } from '../../services/movies/movies.service'
+import { Observable } from 'rxjs/internal/Observable'
+import { Genre, GenresDTO } from '../../types/movie'
 
 @Component({
   selector: 'app-genres',
   templateUrl: './genres.component.html',
-  styleUrl: './genres.component.scss'
+  styleUrl: './genres.component.scss',
 })
-export class GenresComponent {
+export class GenresComponent implements OnInit{
 
-  
+  genres$: Observable<Genre[]> | null = null
+
+  constructor(private moviesService: MoviesService) {}
+
+  ngOnInit(): void {
+    this.genres$ = this.moviesService.getMoviesGenres();
+  }
+
 }
